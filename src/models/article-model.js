734ff0eab8f62146c0efacdf9a2732dc4616051c
azu <azuciao@gmail.com>
2015-01-30 22:53:13 +0900

    initial commit 

---
+++ src/models/article-model.js
@@ -0,0 +1,15 @@
+// LICENSE : MIT
+"use strict";
+export class ArticleModel {
+    constructor() {
+        this.title = "mauriciosoares/core.js";
+        this.url = "https://github.com/mauriciosoares/core.js";
+        this.content = "説明文";
+        this.tags = [
+            "JavaScript",
+            "library"
+        ];
+        this.date = new Date("2014-12-31T21:50:53.755Z");
+        this.relatedLinks = []
+    }
+}
---
+++ src/models/article-related-model.js
@@ -0,0 +1,8 @@
+// LICENSE : MIT
+"use strict";
+export class ArticleRelatedModel {
+    constructor() {
+        this.title = "mauriciosoares/core.js";
+        this.url = "https://github.com/mauriciosoares/core.js";
+    }
+}
